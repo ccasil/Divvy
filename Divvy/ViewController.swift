952afbf6fb3cda1e1138c4ad94aba38b09fefb04
ccasil <cesarkylecasil@gmail.com>
2019-12-16 21:23:06 -0800

    Initial Commit

---
+++ Divvy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Divvy
+//
+//  Created by Cesar Kyle Casil on 12/16/19.
+//  Copyright © 2019 Cesar Kyle Casil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
